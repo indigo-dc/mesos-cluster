8a638c0bc68c6a5d44eeb993a9ba7c7079faa6c4
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-05-18 11:24:25 +0200

    Installing recipes from ansible-galaxy (indigo-dc)

--- heat-templates-v2/setup.sh
+++ heat-templates-v2/setup.sh
@@ -8,6 +8,9 @@ apt-get install -y software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get install -y ansible
-git clone https://github.com/indigo-dc/mesos-deploy.git /tmp/mesos-deploy
-cp -r /tmp/mesos-deploy/ansible/* /etc/ansible/
 
+# Install ansible roles
+echo $role_list
+for role in $role_list; do 
+  ansible-galaxy install indigo-dc.$role;
+done 
