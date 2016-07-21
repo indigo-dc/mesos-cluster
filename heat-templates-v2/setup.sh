0edad3081c8fc8a52325e5452fb173d4fb2c99d5
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-07-21 08:46:04 +0200

    /etc/hosts handling moved inside ansible playbooks

--- heat-templates-v2/setup.sh
+++ heat-templates-v2/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-MYIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-# Fix /etc/hosts
-sed -i "s/127.0.1.1/$MYIP/" /etc/hosts
-
 apt-get install -y software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
