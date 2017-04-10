0ca769418412b4264f09ca39ee91e31d824fd555
Manuel Ciangottini <ciangottinim@users.noreply.github.com>
2017-04-10 15:21:28 +0200

    Update setup2.sh

--- heat-templates-v2/setup2.sh
+++ heat-templates-v2/setup2.sh
@@ -5,7 +5,7 @@ apt-get install -y python-paramiko python-netaddr
 apt-get install -y software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
-apt-get install -y ansible
+apt-get install -y ansible=2.2.1.0-1ppa~trusty
 #wget http://launchpadlibrarian.net/291371956/ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
 #dpkg -i ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
 #rm ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
