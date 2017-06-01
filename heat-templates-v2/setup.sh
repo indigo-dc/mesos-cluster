660100036e005411a6d539abe977306a0114f22f
Marica Antonacci <marica.antonacci@ba.infn.it>
2017-06-01 11:07:08 +0200

    Using ansible from ppa

--- heat-templates-v2/setup.sh
+++ heat-templates-v2/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#apt-get install -y software-properties-common
-#apt-add-repository -y ppa:ansible/ansible
-#apt-get update
-#apt-get install -y ansible
+apt-get install -y software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
 
-apt-get install -y python-paramiko python-netaddr
-wget http://launchpadlibrarian.net/291371956/ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
-dpkg -i ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
-rm ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
+#apt-get install -y python-paramiko python-netaddr
+#wget http://launchpadlibrarian.net/291371956/ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
+#dpkg -i ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
+#rm ansible_2.1.1.0-1~ubuntu16.04.1_all.deb
 
 # Install ansible roles
 echo $role_list
