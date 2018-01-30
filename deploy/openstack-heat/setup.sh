249cef16f6ca6f5973fb656ccddc6354e0e9d6f1
dciangot <diego.ciangottini@cern.ch>
2018-01-30 18:12:51 +0100

    fix

--- deploy/openstack-heat/setup.sh
+++ deploy/openstack-heat/setup.sh
@@ -21,9 +21,9 @@ IFS=','
 # ansible roles using git clone instead   #
 # of ansible-galaxy as follows:           #
 ###########################################
-for role in $role_list; do 
-     ansible-galaxy install indigo-dc.$role;
-done
+#for role in $role_list; do 
+#     ansible-galaxy install indigo-dc.$role;
+#done
 
 ###########################################
 # NOTE: you can test a feature branch for #
@@ -31,12 +31,10 @@ done
 # of ansible-galaxy as follows:           #
 ###########################################
 #
-# for role in $role_list; do
-#   if [ "$role" == "mesos" ]; then
-#     git clone https://github.com/indigo-dc/ansible-role-mesos.git -b enable_dvdi_mod /etc/ansible/roles/indigo-dc.mesos
-#   elif [ "$role" == "cms_config" ]; then
-#     git clone https://github.com/indigo-dc/ansible-role-cms_config.git -b new_proxy /etc/ansible/roles/indigo-dc.cms_config
-#   else
-#     ansible-galaxy install indigo-dc.$role;
-#   fi
-# done
+for role in $role_list; do
+   if [ "$role" == "cms_config" ]; then
+     git clone https://github.com/indigo-dc/ansible-role-cms_config.git -b dciangotdev /etc/ansible/roles/indigo-dc.cms_config
+   else
+     ansible-galaxy install indigo-dc.$role;
+   fi
+done
